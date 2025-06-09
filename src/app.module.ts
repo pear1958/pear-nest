a53a737be581dcac23a4836c6525d8cf87a50fa6
zhangzy <zhangzhiyong@klicen.com>
2025-06-09 17:54:13 +0800

    feat: 添加拦截器

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common'
+import { APP_INTERCEPTOR } from '@nestjs/core'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UserModule } from './modules/user/user.module'
+import { DeviceModule } from './modules/device/device.module'
 import { SystemModule } from './modules/system/system.module'
-import { DeviceModule } from './modules/device/device.module';
+import { TransformInterceptor } from './common/interceptor/transform.interceptor'
 
 @Module({
-  imports: [UserModule, SystemModule, DeviceModule],
+  imports: [UserModule, DeviceModule, SystemModule],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService, { provide: APP_INTERCEPTOR, useClass: TransformInterceptor }]
 })
 export class AppModule {}
---
+++ src/common/model/response.model.ts
@@ -0,0 +1,8 @@
+export class Response {
+  static success() {
+    // xxxxxxxxxx
+  }
+  static error() {
+    // xxxxxxxxxx
+  }
+}
