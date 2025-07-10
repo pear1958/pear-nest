11b6f17a47c401ab4b04562a313418d87ba095ad
zhangzy <zhangzhiyong@klicen.com>
2025-07-10 16:56:34 +0800

    feat: email

--- src/modules/tools/tools.module.ts
+++ src/modules/tools/tools.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common'
 import { RouterModule } from '@nestjs/core'
 import { UploadModule } from './upload/upload.module'
 import { StorageModule } from './storage/storage.module'
+import { EmailModule } from './email/email.module'
 
-const modules = [UploadModule, StorageModule]
+const modules = [UploadModule, StorageModule, EmailModule]
 
 @Module({
   imports: [
