4f001a79f1f519fc0a5ce0118ad753e2bb15ea0e
zhangzy <doxoops@163.com>
2025-07-10 23:02:45 +0800

    fix: code

--- src/modules/tools/upload/upload.dto.ts
+++ src/modules/tools/upload/upload.dto.ts
@@ -9,7 +9,38 @@ export class FileUploadDto {
   @IsDefined()
   @IsFile(
     {
-      mimetypes: ['image/png', 'image/gif', 'image/jpeg', 'image/webp', 'image/svg+xml'],
+      mimetypes: [
+        // 图片类
+        'image/jpeg', // .jpg 或 .jpeg
+        'image/png',
+        'image/gif',
+        'image/bmp',
+        'image/svg+xml',
+        'image/webp',
+        // 文档类
+        'text/plain', // .txt
+        'application/msword', // .doc
+        'application/vnd.openxmlformats-officedocument.wordprocessingml.document', // .docx
+        'application/vnd.ms-excel', // .xls
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', // .xlsx
+        'application/vnd.ms-powerpoint', // .ppt
+        'application/vnd.openxmlformats-officedocument.presentationml.presentation', // .pptx
+        'application/pdf', // .pdf
+        // 音频类
+        'audio/mpeg', // .mp3
+        'audio/wav', // .wav
+        'audio/ogg', // .ogg
+        'audio/flac', // .flac
+        // 视频类
+        'video/mp4', // .mp4
+        'video/x-msvideo', // .avi
+        'video/quicktime', // .mov
+        'video/x-matroska', // .mkv
+        // 压缩包类
+        'application/zip', // .zip
+        'application/x-rar-compressed', // .rar
+        'application/x-7z-compressed', // .7z
+      ],
       fileSize: 1024 * 1024 * 10
     },
     {
